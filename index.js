60ac59fa5ded4891cd96456e356e02f9f81f86b5
TaehwanGo <gth1123@naver.com>
2020-12-28 11:15:32 +0900

    #0.4 First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "Tony";
+const age = 24;
+const gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age); // 만약 parameter중 하나라도 빠지면 에러 발생, js에선 발생 안함(undefined인채로 실행 됨)
 //# sourceMappingURL=index.js.map
