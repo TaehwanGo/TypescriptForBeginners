5518a7e74122b0f40056bc27ec650bbcfdb6121b
TaehwanGo <gth1123@naver.com>
2020-12-28 17:12:43 +0900

    #0.8 BlockChain Creating a Block

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,30 @@
-// interface Human{
-//     name:string,
-//     age:number,
-//     gender:string
-// }
-
-// const person = {
-//     name: "Tony",
-//     age: 30,
-//     gender: "male"
-// }
-
-class Human {
-    public name: string;
-    public age: number; // private age: number; 로 바꾸면 class밖에서 person.age 와 같이 접근 할 수 없음
-    public gender: string;
-    constructor(name: string, age: number, gender: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block{
+    public index:number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index:number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const tony = new Human("tony", 31, "male");
+const genesisBlock:Block = new Block(0, "20202020", "", "Hello", 123456);
 
-const sayHi = (person: Human):string => {
-    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
-}
+let blockChain: [Block] = [genesisBlock];
 
-console.log(sayHi(tony)); // argument의 type이 맞지 않으면 에러 발생
+// blockChain.push("stuff"); // 이런 실수를 막아주는게 typescript
+
+console.log(blockChain);
 
 export{};
