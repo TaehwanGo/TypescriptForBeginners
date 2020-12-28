dd5a44f1f45e0eb2fecc1432d55fd0ab0ff653fb
TaehwanGo <gth1123@naver.com>
2020-12-28 15:47:00 +0900

    #0.6 Interfaces on Typescript

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-// const name = "Tony";
-// const age = 24;
-// const gender = "male";
-
-// const sayHi = (name:string, age:number, gender:string):void => {
-//     console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
-// }
-
-const sayHi = (name:string, age:number, gender:string):string => {
-    return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human{
+    name:string,
+    age:number,
+    gender:string
 }
 
-console.log(sayHi("Tony", 30, "mail")); // argument의 type이 맞지 않으면 에러 발생
+const person = {
+    name: "Tony",
+    age: 30,
+    gender: "male"
+}
+
+const sayHi = (person: Human):string => {
+    return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}!`;
+}
+
+console.log(sayHi(person)); // argument의 type이 맞지 않으면 에러 발생
 
 export{};
